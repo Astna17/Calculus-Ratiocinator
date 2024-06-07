9657d727eb7d91ef9876bb798b1a32ecf80bd9b8
Astna17 <hei.antsa.17@gmail.com>
2024-06-07 13:21:38 +0300

    first commit

---
+++ src/test/java/com/calculus/ratiocinator/RatiocinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculus.ratiocinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatiocinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
